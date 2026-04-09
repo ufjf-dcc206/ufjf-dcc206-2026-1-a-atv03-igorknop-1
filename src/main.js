fee11938e540f2b5c2901f802b1d044567ff826f
Igor Knop <igor.knop@ufjf.br>
2026-04-08 22:18:09 -0300

    :sparkles: adiciona root

--- src/main.js
+++ src/main.js
@@ -1,9 +1,4 @@
 import './style.css'
-import { setupCounter } from './counter.js'
 import { setupRootApp } from './setupRootApp.js'
 
-document.querySelector('#app').innerHTML = setupRootApp();
-
-
-
-setupCounter(document.querySelector('#counter'))
+document.querySelector('#app').append(setupRootApp());
