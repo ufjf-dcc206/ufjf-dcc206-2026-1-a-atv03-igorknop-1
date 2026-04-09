24d83a6484d748e459975ca9c09cd3afd4b40fa2
Igor Knop <igor.knop@ufjf.br>
2026-04-08 22:09:20 -0300

    :feature::wrench: adiciona limpa e iniciar lista

--- src/lista.js
+++ src/lista.js
@@ -1,5 +1,13 @@
-const lista = ["um", "dois", "três"];
+let lista = [];
+
+export function iniciaLista(){
+    lista = ["um", "dois", "três"];
+}
 
 export function getLista(){
     return structuredClone(lista);
+}
+
+export function limpaLista(){
+    lista.splice(0);
 }
--- src/lista.test.js
+++ src/lista.test.js
@@ -1,15 +1,26 @@
-import {expect, it} from "vitest";
-import { getLista } from "./lista";
+import { expect, it } from "vitest";
+import { getLista, iniciaLista, limpaLista } from "./lista";
 
-it("deve ter elementos iniciais", ()=>{
-    const lista = getLista();
-    expect(lista.length).toBe(3);
+it("deve ter zero elementos após limpar a lista", () => {
+  iniciaLista();
+  const lista1 = getLista();
+  expect(lista1.length).toBe(3);
+  limpaLista();
+  const lista2 = getLista();
+  expect(lista2.length).toBe(0);
 });
 
-it("alterar a lista retornada, não pode mexer na do módulo", ()=>{
-    const lista1 = getLista();
-    expect(lista1.length).toBe(3);
-    lista1.push("quatro");
-    const lista2 = getLista();
-    expect(lista2.length).toBe(3);
+it("deve ter elementos iniciais", () => {
+  iniciaLista();
+  const lista = getLista();
+  expect(lista.length).toBe(3);
+});
+
+it("alterar a lista retornada, não pode mexer na do módulo", () => {
+  iniciaLista();
+  const lista1 = getLista();
+  expect(lista1.length).toBe(3);
+  lista1.push("quatro");
+  const lista2 = getLista();
+  expect(lista2.length).toBe(3);
 });
